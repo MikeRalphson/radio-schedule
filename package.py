5252ce2d6c7973e5834dd11119978b40422fa426
Nicholas Humfrey <njh@aelius.com>
2016-01-04 18:11:37 +0000

    Implemented MBT based packaging of the ruby app

---
+++ package.py
@@ -0,0 +1,38 @@
+def package(mbt):
+
+    source_file = mbt.name + '.tar.gz'
+
+    # source prepare
+    # FIXME: the exclude list is a bit yucky
+    mbt.run([
+      'tar', '-czf', mbt.sources_dir + '/' + source_file,
+      '--exclude-vcs',
+      '--exclude=.DS_Store',
+      '--exclude=package.py',
+      '--exclude=package.pyc',
+      '--exclude=project.json',
+      '--exclude=SOURCES',
+      '--exclude=RPMS',
+      '--exclude=SRPMS',
+      '--exclude=SPECS',
+      '.'
+    ], cwd=mbt.root + '/')
+
+    mbt.spec.add_source(source_file)
+    mbt.spec.set_build_arch(None)
+    mbt.spec.set_prep('%setup -c -n ' + mbt.name)
+
+    mbt.spec.add_build_steps([
+        ['scl', 'enable', 'ruby193', '"bundle install --deployment"'],
+    ])
+
+    app_dir = "$RPM_BUILD_ROOT/usr/lib/" + mbt.name + '/'
+    mbt.spec.add_install_steps([
+        ['rm', '-rf', '$RPM_BUILD_ROOT'],
+        ['mkdir', '-p', app_dir],
+        ['cp', '-rf', '.', app_dir]
+    ])
+
+    # %files
+    mbt.spec.add_files(["/usr/lib/" + mbt.name], file_permissions=644, dir_permissions=755)
+
