000fa233c05b47b558baebe29ac937c23327cab3
Nicholas Humfrey <njh@aelius.com>
2016-01-18 14:59:23 +0000

    Custom service installs bake-scripts automatically - don't need to do this ourselves

--- package.py
+++ package.py
@@ -32,14 +32,11 @@ def package(mbt):
         ["rm", "-rf", "%{buildroot}/*"],
         ["mkdir", "-p", app_dir],
         ["mkdir", "-p", "%{buildroot}%{_initddir}"],
-        ["mkdir", "-p", "%{buildroot}/etc/bake-scripts"],
         ["mv", "initd.sh", "%{buildroot}%{_initddir}/" + mbt.name],
-        ["mv", "bake-scripts/export-config", "%{buildroot}/etc/bake-scripts"],
         ["cp", "-rf", ".", app_dir]
     ])
 
     # %files
-    mbt.spec.add_files(["/etc/bake-scripts"], file_permissions=755)
     mbt.spec.add_files(["%{_initddir}"], file_permissions=755)
     mbt.spec.add_files(["/usr/lib/" + mbt.name], file_permissions=644, dir_permissions=755)
     mbt.spec.add_files(["/usr/lib/" + mbt.name + '/start.sh'], file_permissions=755)
